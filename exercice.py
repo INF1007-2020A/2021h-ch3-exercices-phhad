242108b6c578a2943fb6bb67aa6bef9374166778
phhad <philippe.hadjis@polymtl.ca>
2021-01-26 14:33:32 -0500

    FIN

--- exercice.py
+++ exercice.py
@@ -25,8 +25,11 @@ def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float
 
 
 def to_degrees(angle_rads: float) -> tuple:
-    degrees = (angle_rads * (180/math.pi()))
-    return degrees
+    degrees = math.degrees(angle_rads)
+    min = (degrees - math.floor(degrees)) * 60
+    sec = (min - math.floor(min)) * 60
+
+    return math.floor(degrees), math.floor(min), sec
 
 
 def to_celsius(temperature: float) -> float:
